022fa59e1033fa89a41014ee7df1ad7369c51fb6
Pat Thomas <pthomas@relaynetwork.com>
2015-06-11 10:10:42 -0400

    cleanup

--- src/egg/dsl.clj
+++ src/egg/dsl.clj
@@ -8,16 +8,4 @@
         route-namespaces (h/find-cljs-namespaces-for-prefix routes)]
     `(do
        ~@(for [route-ns# route-namespaces]
-           `(goog/require ~(h/namespace->goog-require-str route-ns#)))
-       #_@(for [route-ns# route-namespaces]
-            `(do
-               (cljs.core/in-ns ~route-ns#)
-               :foof))
-       #_`(cljs.core/in-ns ~current-ns))))
-
-(comment
-  (macroexpand-1
-   '(defapp foof
-      {:routes :routes}))
-  (h/find-cljs-namespaces-for-prefix :routes)
-  )
+           `(goog/require ~(h/namespace->goog-require-str route-ns#))))))
